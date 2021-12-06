55a7e82619691fd40d115c0cc4169c85ca2e408e
t1dmlgus <dmlgusgngl@gmail.com>
2021-12-06 10:09:10 +0900

    feat: 도메인 모델 도출(배송정보 데이터) - 요구사항 6
    - 배송지 정보는 받는 사람 이름, 전화번호, 주소로 구성된다.

--- src/main/java/com/s1dmlgus/ddd/order/ShippingInfo.java
+++ src/main/java/com/s1dmlgus/ddd/order/ShippingInfo.java
@@ -1,4 +1,17 @@
 package com.s1dmlgus.ddd.order;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
 public class ShippingInfo {
+
+    private String receiverName;
+    private String receiverPhoneNumber;
+    private String shippingAddr01;
+    private String shippingAddr02;
+    private String shippingAddr03;
+
 }
